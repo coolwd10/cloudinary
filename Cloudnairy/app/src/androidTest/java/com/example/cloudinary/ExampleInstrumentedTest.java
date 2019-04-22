fe173a21f467c0217bb9823959c9e43ed6c884a4
Akash Shah <akashshah@gmx.com>
2019-04-22 07:07:00 +0530

    Added UI test cases

--- Cloudnairy/app/src/androidTest/java/com/example/cloudinary/ExampleInstrumentedTest.java
+++ Cloudnairy/app/src/androidTest/java/com/example/cloudinary/ExampleInstrumentedTest.java
@@ -2,11 +2,21 @@ package com.example.cloudinary;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.cloudinary.android.mvp.main.home.HomeActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +26,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule =
+            new ActivityTestRule<>(HomeActivity.class);
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +37,33 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.nytimes", appContext.getPackageName());
     }
+
+    @Test
+    public void testCase1(){
+        onView(withId(R.id.btn)).perform(click());
+    }
+
+    @Test
+    public void testCase2(){
+        onView(withId(R.id.btn)).perform(click());
+        onView(withText("Cancel")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCase3(){
+        onView(withId(R.id.btn)).perform(click());
+        onView(withText("Take Photo")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCase4(){
+        onView(withId(R.id.btn)).perform(click());
+        onView(withText("Choose from Gallery")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCase5(){
+        onView(withId(R.id.upload)).perform(click());
+    }
+
 }
